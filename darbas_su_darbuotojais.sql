c17ae1e38292e46b95ed66b4d44ebdf69192d5e0
simonasaudickas <simonas.audickas@gmail.com>
2022-02-05 22:31:11 +0200

    accepting the changes

--- darbas_su_darbuotojais.sql
+++ darbas_su_darbuotojais.sql
@@ -1,9 +1,7 @@
 select * from darbuotojai;
 
 
-<<<<<<< HEAD
-Delete from darbuotojai_kopija where vardas = 'Vincentas';
-=======
+
 delete from darbuotojai where vardas = 'Vincentas';
 
 /* Amelijos pakeitimai */
@@ -17,4 +15,3 @@ create table if not exist 'uzduotys' (
 insert or ignore into uzduotys (pavadinimas, pastaba, iki_data) values ('Programuoti', 'Programuoti Python kalba', 1644522987), ('Gerti kava', 'Išgerti šiek tiek kavos', 1644526587), ('Toliau programuoti', 'Toliau tęsti darbą', 1644530187);
 
 SELECT pavadinimas, pastaba, datetime(iki_data, 'unixepoch', 'localtime') as padaryti_iki FROM uzduotys;
->>>>>>> a0e939108231b01b66ec284bcfc5e9934bcdff66
